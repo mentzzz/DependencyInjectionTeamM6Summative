c3dc345cf134a811c483eacb87af91251c34d417
Maria Wong <maria@Marias-MacBook-Pro.local>
2019-07-15 14:08:08 -0400

    adds controller files

--- m6summative/src/main/java/com/dependencyinjection/m6summative/controller/CustomerController.java
+++ m6summative/src/main/java/com/dependencyinjection/m6summative/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.dependencyinjection.m6summative.controller;
 import com.dependencyinjection.m6summative.dto.Customer;
 import com.dependencyinjection.m6summative.service.ServiceLayer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,26 +16,34 @@ public class CustomerController {
     private ServiceLayer service;
 
     @RequestMapping(value="/customer", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.CREATED)
     public Customer createCustomer(@RequestBody Customer customer) {
         return service.saveCustomer(customer);
     }
 
     @RequestMapping(value="/customer/{id}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
     public Customer getCustomerById(@PathVariable(name="id") int id ) {
         return service.findCustomer(id);
     }
 
     @RequestMapping(value="/customers", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
     public List<Customer> getAllCustomers() {
         return service.findAllCustomers();
     }
 
     @RequestMapping(value="/customer/{id}", method = RequestMethod.PUT)
-    public void updateCustomer(@RequestBody Customer customer ) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateCustomer(@RequestBody Customer customer, @PathVariable(name = "id") int id ) {
+        if (id != customer.getCustomerId()) {
+            throw new IllegalArgumentException("Customer ID on path must match the ID in the Customer object.");
+        }
         service.updateCustomer(customer);
     }
 
     @RequestMapping(value="/customer/{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteCustomer(@PathVariable(name="id") int id ) {
         service.removeCustomer(id);
     }
